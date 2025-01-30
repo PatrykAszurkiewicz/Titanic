437122614266f9b104ea24f61cd9953c85e7bd9e
ActiveP <csgorandomly@gmail.com>
2025-01-30 19:49:30 +0100

    Test

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+titanic = pd.read_csv('train.csv')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("siema")
-
-for i in range(1, 10):
-    print("test")
+print(titanic)
